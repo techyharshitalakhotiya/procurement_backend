be62ddf0d7a46792cf7decaf02f67f76a98023af
Harshita Lakhotiya <harshitalakhotiya@gmail.com>
2026-04-28 12:14:25 +0530

    enable CORS for frontend connection for college project

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
